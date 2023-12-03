c2813c596597780e9802f86376e257dfbcfc7a81
ntnhat267 <90461453+ntnhat267@users.noreply.github.com>
2023-12-03 16:32:41 +0700

    feat: add QueryClientProvider, BrowserRouter, ErrorBoundary and ReactQueryDevtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ErrorBoundary } from 'react-error-boundary';
+import { BrowserRouter } from 'react-router-dom';
+
+import FallbackRenderer from './components/FallbackRenderer';
+import Router from './router';
+
 import './App.css';
-// import { Button } from "@/components/ui/button"
 
-// export default function Home() {
-//   return (
-//     <div>
-//       <Button>Click me</Button>
-//     </div>
-//   )
-// }
-
+const queryClient = new QueryClient();
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ErrorBoundary fallbackRender={FallbackRenderer}>
+          <Router />
+        </ErrorBoundary>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
