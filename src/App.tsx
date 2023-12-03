8ae4948d5b56187c19df9ec6292e1fee2c62720b
ntnhat267 <90461453+ntnhat267@users.noreply.github.com>
2023-12-03 12:25:27 +0700

    feat: add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+// import { Button } from "@/components/ui/button"
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <Button>Click me</Button>
+//     </div>
+//   )
+// }
 
 function App() {
   const [count, setCount] = useState(0);
