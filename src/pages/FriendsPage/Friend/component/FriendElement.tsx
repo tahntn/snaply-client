02cf6490ce4ee862b1da932434d8671cde0fd1d7
quangvu0905 <superquang08@gmail.com>
2024-01-15 17:25:09 +0700

    ui-friend-alphabet

--- src/pages/FriendsPage/Friend/component/FriendElement.tsx
+++ src/pages/FriendsPage/Friend/component/FriendElement.tsx
@@ -1,6 +1,14 @@
 import { Box, Text } from '@radix-ui/themes';
 import { cn } from '@/lib/utils';
 import AvatarUser from '@/components/AvatarUser';
+import { Ban, MoreVertical, Pencil, Trash2 } from 'lucide-react';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 interface FriendElementProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -11,15 +19,34 @@ const FriendElement: React.FC<FriendElementProps> = ({ friend }) => {
   const { avatar, username } = friend;
 
   return (
-    <Box className={cn('w-full cursor-pointer relative')}>
-      <Box className="flex flex-row items-center gap-4">
-        <AvatarUser name={username ?? ''} url={avatar ?? ''} className="rounded-none" />
+    <Box className={cn('w-full cursor-pointer relative flex items-center justify-between')}>
+      <Box className="flex flex-row items-center gap-2">
+        <AvatarUser name={username ?? ''} url={avatar ?? ''} />
         <Box className="flex flex-col gap-[0.2px]">
-          <Text className="text-base font-semibold truncate max-w-[320px] opacity-0 lg:opacity-100">
+          <Text className="text-sm font-medium truncate max-w-[320px] opacity-0 lg:opacity-100">
             {username ?? ''}
           </Text>
         </Box>
       </Box>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <MoreVertical className="h-5" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="left-[3%]">
+          <DropdownMenuItem className="flex justify-between cursor-pointer">
+            Edit
+            <Pencil className="h-5" />
+          </DropdownMenuItem>
+          <DropdownMenuItem className="flex justify-between cursor-pointer">
+            Block
+            <Ban className="h-5" />
+          </DropdownMenuItem>
+          <DropdownMenuItem className="flex justify-between cursor-pointer text-red-400 focus:text-red-400">
+            Remove
+            <Trash2 className="h-5" />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </Box>
   );
 };
